6ce34a7730c744dabb3341ae37fdae5bb122ceb7
David <learnitmyway@users.noreply.github.com>
2020-05-30 10:37:34 +0200

    replace boilerplate with button

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent {
+  @Input() style: { backgroundColor: string };
+}
