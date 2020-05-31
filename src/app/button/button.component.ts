64b79a1f55c1e2c22a297e8a4da533843667bab5
David <learnitmyway@users.noreply.github.com>
2020-05-31 10:39:50 +0200

    Revert "hard code button background"
    This reverts commit 581875df9bd41ef52c763af634a72c3658d0a6f6.

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -5,4 +5,6 @@ import { Component, Input } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css'],
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input() style: { [index: string]: string };
+}
